51bbdc685789db18c5c75c6fe79567b309808df8
Stephanie D <slr14404@yahoo.com>
2017-11-28 01:20:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
-function scuberGreetingForFeet() {
-  if (scuberGreetingForFeet <= 400)
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400)
   return "This one is on me!"
 }
