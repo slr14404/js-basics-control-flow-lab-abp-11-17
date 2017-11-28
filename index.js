de2109e4bf28d7077982c8e7de71712f1c905f7c
Stephanie D <slr14404@yahoo.com>
2017-11-28 01:50:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 // Write your code in this file!
-function scuberGreetingForFeet (distance) {
+function scuberGreetingForFeet(distance) {
   if (distance <= 400) {
     return "This one is on me!"
   }
-  else if  (distance > 2000 && distance <= 2500) {
+  else if (distance > 2000 && distance <= 2500) {
     return "I will gladly take your thirty bucks."
   }
   else if (distance > 2500) {
     return "No can do."
   }
 }
-
 function ternaryCheckCity (city) {
   return (city === "NYC") ? "Ok, sounds good." : "No go."
 }
-
-function switchOnCharmFromTip(tipString){
-  switch(tipString){
+function switchOnCharmFromTip(tipstring) {
+  switch(tipstring){
   case "generous":
-    return "Thank you so much."
+     return "Thank you so much."
   case "not as generous":
     return "Thank you."
   default:
